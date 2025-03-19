7e1dd2cfbf2b0ea048dec11e8693f2588caf4a29
Maxamed Cigaal <lovemoha47@gmail.com>
2025-03-19 21:45:00 +0300

    new file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,5 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSumarry.js";
 
 renderOrderSummary();
+renderPaymentSummary();

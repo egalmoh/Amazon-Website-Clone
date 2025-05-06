c77f4d033ca92882b06b2efc121aecaa025523a0
Maxamed Cigaal <lovemoha47@gmail.com>
2025-05-06 17:48:03 +0300

    added and updated progress bar to semulate real time

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -1,16 +1,15 @@
 import { getProduct, loadProductsFetch} from '../data/products.js';
 import { cart } from '../data/cart-class.js';
 import { updateCartQuantity } from './amazon.js';
+import { orders } from './orders.js'
 
 const url = new URL(window.location.href);
-console.log(url.searchParams.get('orderId'));
-console.log(url.searchParams.get('productId'));
-console.log(url.searchParams.get('deliveryTime'));
-console.log(url.searchParams.get('quantity'));
 
+const orderId = url.searchParams.get('orderId')
 const productId = url.searchParams.get('productId');
 const estimatedDeliveryTime = url.searchParams.get('deliveryTime')
 const quantity = url.searchParams.get('quantity')
+console.log(orderId)
 
 
 async function renderOrders() {
@@ -18,10 +17,6 @@ async function renderOrders() {
   await loadProductsFetch();
 
   const matchingProduct = getProduct(productId);
-  console.log(matchingProduct)
-
-  // const matchingCartProduct = getCartProduct(productId);
-  // console.log(matchingCartProduct)
 
   const date = new Date(estimatedDeliveryTime);
   const readableDate = date.toLocaleDateString('en-US', {
@@ -49,25 +44,89 @@ async function renderOrders() {
 
     <img class="product-image" src="${matchingProduct.image}">
 
-    <div class="progress-labels-container">
-      <div class="progress-label">
-        Preparing
-      </div>
-      <div class="progress-label current-status">
-        Shipped
-      </div>
-      <div class="progress-label">
-        Delivered
-      </div>
-    </div>
-
-    <div class="progress-bar-container">
-      <div class="progress-bar"></div>
-    </div>
+    ${progressBar()}
   `;
 
   document.querySelector('.js-order-tracking').innerHTML = trackingHTML;
 };
+
+function progressBar() {
+
+  console.log('hello')
+
+  let progressBarHTML = '';
+  
+  let orderTime;
+
+  orders.forEach(order => {
+    if (orderId === order.id) {
+      orderTime = order.orderTime;
+    }
+  });
+
+  const currentTime = new Date(); // Current time
+  const deliveryTime = new Date(estimatedDeliveryTime); // Delivery time from URL
+  orderTime = new Date(orderTime);  // Order time
+
+
+  const percentProgress = Math.min(Math.max(((currentTime.getTime() - orderTime.getTime()) / (deliveryTime.getTime() - orderTime.getTime())) * 100, 0), 100); // Ensure percentage is between 0 and 100
+  console.log(percentProgress )
+
+  if (percentProgress >= 0 && percentProgress <= 49) {
+    progressBarHTML = `
+      <div class="progress-labels-container">
+        <div class="progress-label current-status">
+          Preparing
+        </div>
+        <div class="progress-label">
+          Shipped
+        </div>
+        <div class="progress-label">
+          Delivered
+        </div>
+      </div>
+      <div class="progress-bar-container">
+        <div class="progress-bar" style="width:${percentProgress}%;"></div>
+      </div>
+    `
+  } else if (percentProgress >= 50 && percentProgress <= 99) {
+    progressBarHTML = `
+      <div class="progress-labels-container">
+        <div class="progress-label">
+          Preparing
+        </div>
+        <div class="progress-label current-status">
+          Shipped
+        </div>
+        <div class="progress-label">
+          Delivered
+        </div>
+      </div>
+      <div class="progress-bar-container">
+        <div class="progress-bar" style="width:width:${percentProgress}%;"></div>
+      </div>
+    `
+  } else if (percentProgress === 100) {
+    progressBarHTML = `
+      <div class="progress-labels-container">
+        <div class="progress-label">
+          Preparing
+        </div>
+        <div class="progress-label">
+          Shipped
+        </div>
+        <div class="progress-label current-status">
+          Delivered
+        </div>
+      </div>
+      <div class="progress-bar-container">
+        <div class="progress-bar" style="width:width:${percentProgress}%;"></div>
+      </div>
+    `
+  }
+
+  return progressBarHTML
+}
 
 
 document.addEventListener('DOMContentLoaded', () => {
